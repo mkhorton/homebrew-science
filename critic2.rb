f3e0dd9c151033b88768719d6de921d858ecbf8b
Matthew Horton <mkhorton@users.noreply.github.com>
2017-08-17 15:01:45 -0700

    Update critic2.rb, tidy whitespace for audit

--- critic2.rb
+++ critic2.rb
@@ -5,17 +5,16 @@ class Critic2 < Formula
   version "stable"
   sha256 "5c0803d47fbbb061b9f0b44e56eeebd7aa3298b275b9253fecdae7847a1ff903"
 
+  option "with-libxc", "Compile with libxc support to calculate exchange and correlation densities"
+
   depends_on "gcc" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-
-  option "with-libxc", "Compile with libxc support to calculate exchange and correlation densities"  
   depends_on "libxc" => :optional
-  
+
   def install
-    
     system "autoreconf", "-i"
-                          
+
     if build.with? "libxc"
       system "./configure", "--with-libxc-prefix=#{HOMEBREW_PREFIX}/lib",
                             "--with-libxc-include=#{HOMEBREW_PREFIX}/include",
@@ -34,7 +33,6 @@ class Critic2 < Formula
 
     system "make"
     system "make", "install"
-    
   end
 
   test do
